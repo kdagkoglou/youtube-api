e403428c9d983d5a82ec6b8f3b8d0d6a02a06601
Kostas Dagkoglou <kostas.dagkoglou@empello.com>
2019-11-06 16:23:41 +0000

    Updates

---
+++ precache-manifest.0876bc9ceb4a1fe00fbbd6939ff97f87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45c491412164eec44f1b15475f41b00",
+    "url": "/youtube-api/index.html"
+  },
+  {
+    "revision": "4b278084ae516b010e39",
+    "url": "/youtube-api/static/css/main.8a23e6ba.chunk.css"
+  },
+  {
+    "revision": "49f5d74b87fa37f5bcd8",
+    "url": "/youtube-api/static/js/2.3bb4e91f.chunk.js"
+  },
+  {
+    "revision": "4b278084ae516b010e39",
+    "url": "/youtube-api/static/js/main.c6eef3c5.chunk.js"
+  },
+  {
+    "revision": "23b76554a3a6c9d61d11",
+    "url": "/youtube-api/static/js/runtime-main.6427710e.js"
+  }
+]);
